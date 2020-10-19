6b00c22ad5466683395bd4b1c853c7cab9096bc0
Neeraj Gupta <gneeraj32595@gmail.com>
2020-10-19 19:39:46 +0530

    leaderboard added

--- static/js/script.js
+++ static/js/script.js
@@ -1,62 +1,91 @@
+// jshint esversion:6
+
 // Added Preloader
 function preloader() {
   var preloader = document.getElementById("preloader");
   preloader.style.display = "none";
 }
 
+// API Fetching
+const url = "http://api.ishandeveloper.com/30days";
+fetch(url).then(function (response) {
+	// The API call was successful!
+	if (response.ok) {
+		return response.json();
+	} else {
+		return Promise.reject(response);
+	}
+}).then(function (data) {
+  // This is the JSON from our response
+  // console.log(data.stats.length);
+  // console.log(data.stats);
+  duplicateCards(data.stats.length, data.stats);
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
 
-// Replicating divisions
 
-for(let i  = 0; i < 42; i++) {
-  var parent = document.querySelector(".parent-wrapper");
-  var newDivision = document.createElement("div");
-  const card = `<div class="d-flex flex-row">
-  <div
-    class="d-inline-flex px-5 justify-content-center align-items-center child-serial-no"
-  >
-    1
-  </div>
-  <div class="d-inline-flex flex-column">
-    <div class="d-flex child-player-name">Player 1</div>
-    <div class="d-flex">5 star</div>
-  </div>
-  <div class="d-inline-flex ml-auto mr-5 trophy">
-    <img
-      src="images/trophy.png"
-      alt="Trophy"
-      class="child-trophy-players"
-    />
-  </div>
-</div>`;
-  newDivision.innerHTML += card;
-  newDivision.className = "col-10 offset-1 child py-3 mt-5";
-  parent.append(newDivision);
+// Find Stars
+function findStars(numberOfQuests) {
+  let totalNumberOfQuests = 16;
+  let stars = Math.ceil((numberOfQuests/totalNumberOfQuests) * 5);
+  // console.log(stars);
+  return stars;
+}
+
+// Captilize First Letter
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 
+// Replicating divisions
+
+function duplicateCards(numberOfCards, dataArray) {
+  for (let i = 0; i < numberOfCards; i++) {
+    let rank = dataArray[i].rank;
+    // console.log(dataArray[i]);
+    let name = capitalizeFirstLetter(dataArray[i].name);
+    let quests = dataArray[i].quests;
+    let labs = dataArray[i].labs;
+    let numberOfStars = findStars(quests);
+    var parent = document.querySelector(".parent-wrapper");
+    var newDivision = document.createElement("div");
+    const card = `<div class="d-flex flex-row">
+    <div
+      class="d-inline-flex px-5 justify-content-center align-items-center child-serial-no"
+    >
+      ${rank}
+    </div>
+    <div class="d-inline-flex flex-column">
+      <div class="d-flex child-player-name">${name}</div>
+      <div class="d-flex">${numberOfStars}</div>
+    </div>
+    <div class="d-inline-flex ml-auto mr-5 trophy">
+      <img
+        src="static/images/trophy.png"
+        alt="Trophy"
+        class="child-trophy-players"
+      />
+    </div>
+  </div>`;
+    newDivision.innerHTML += card;
+    newDivision.className = "col-10 offset-1 child py-3 mt-5";
+    parent.append(newDivision);
+    assignColorToCards();
+  }
+  
+}
+
 
 // Assign Random colors to divisions
-
-// var colors = [ "#00ffff", "#f0ffff", "#f5f5dc", "#000000", "#0000ff", "#a52a2a", "#00ffff", "#00008b", "#008b8b", "#a9a9a9", "#006400", "#bdb76b", "#8b008b", "#556b2f", "#ff8c00", "#9932cc", "#8b0000", "#e9967a", "#9400d3", "#ff00ff", "#ffd700", "#008000", "#4b0082", "#f0e68c", "#add8e6", "#e0ffff", "#90ee90", "#d3d3d3", "#ffb6c1", "#ffffe0", "#00ff00", "#ff00ff", "#800000", "#000080", "#808000", "#ffa500", "#ffc0cb", "#800080", "#800080", "#ff0000", "#c0c0c0", "#ffffff", "#ffff00" ];
-
-// var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-// 		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-// 		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-// 		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-// 		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-// 		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-// 		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-// 		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-// 		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-//       '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 var colors = [];
-      // Generate random colors
-      for(var i=0; i<1000; i++){
-        var color ='#'+Math.random().toString(16).substr(2,6);
-        colors.push(color);
-      }
-
-// var colors = ['red', 'green', 'blue', 'orange', 'purple', 'GoldenRod', 'Indigo','LightSeaGreen', 'MidnightBlue', 'Teal', 'MediumVioletRed', 'LightSlateGray'];
+// Generate random colors
+for (var i = 0; i < 1000; i++) {
+  var color = '#' + Math.random().toString(16).substr(2, 6);
+  colors.push(color);
+}
 
 function randomcolors() {
   var randomNumber = Math.floor(Math.random() * colors.length);
@@ -64,22 +93,25 @@ function randomcolors() {
   return colors[randomNumber];
 };
 
+function assignColorToCards() {
+
 var divisionList = document.querySelectorAll(".child");
 
-for(let i = 0;  i < divisionList.length; i ++ ) {
+for (let i = 0; i < divisionList.length; i++) {
   // console.log(i);
   divisionList[i].style.backgroundColor = randomcolors();
+}
 }
 
 
 // Breakpoints
-$(window).resize(function () { 
+$(window).resize(function () {
   // console.log(window.innerWidth);
   let playerList = document.querySelectorAll(".child-player-name");
   let playerNumber = document.querySelectorAll(".child-serial-no");
   let trophy = document.querySelectorAll(".trophy");
-  if(window.innerWidth <= 605) {
-    for(let i = 0; i < playerList.length; i++) {
+  if (window.innerWidth <= 605) {
+    for (let i = 0; i < playerList.length; i++) {
       playerList[i].classList.add("mt-3");
       playerList[i].style.fontSize = "30px";
       playerNumber[i].style.fontSize = "30px";
@@ -87,10 +119,10 @@ $(window).resize(function () {
       playerNumber[i].classList.add("px-3");
       trophy[i].classList.remove("mr-5");
       trophy[i].classList.add("mr-0");
-      
+
     }
   } else {
-    for(let i = 0; i < playerList.length; i++) {
+    for (let i = 0; i < playerList.length; i++) {
       playerList[i].classList.remove("mt-3");
       playerList[i].style.fontSize = "50px";
       playerNumber[i].style.fontSize = "50px";
@@ -101,7 +133,5 @@ $(window).resize(function () {
     }
   }
 });
-
-
 
 
